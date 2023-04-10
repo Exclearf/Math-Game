8bf3f5a22ffee5ba50fae20ee41624028cfff067
Exclearf <nboholii@gmail.com>
2023-04-10 22:16:35 +0200

    Added a random operations option. Added a timer for each game. Added a history of all tries.

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -4,6 +4,8 @@
     {
         private static int DifficultyLevel { get; set; }
         public static int NumberOfQuestions { get; set; }
+        public static List<string> Answers = new();
+        public static DateTime startTime;
 
         static void Main(string[] args)
         {
@@ -11,39 +13,69 @@
 
             var date = DateTime.Now;
             var name = Console.ReadLine();
-            ShowMenu(date, name);
+            while (true)
+            {
+                Console.Clear();
+                ShowMenu(date, name);
 
-            var choice = Console.ReadLine();
+                var choice = Console.ReadLine();                
 
+                switch (choice.Trim().ToLower())
+                {
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        Environment.Exit(1);
+                        break;
+                    case "q":
+                        Console.WriteLine("Was nice having you here!");
+                        Environment.Exit(0);
+                        break;
+                    case "a":
+                        GetComplexityAndAmount();
+                        Addition();
+                        break;
+                    case "s":
+                        GetComplexityAndAmount();
+                        Substraction();
+                        break;
+                    case "m":
+                        GetComplexityAndAmount();
+                        Multiplication();
+                        break;
+                    case "d":
+                        GetComplexityAndAmount();
+                        Division();
+                        break;
+                    case "r":
+                        GetComplexityAndAmount();
+                        RandomOperators();
+                        break;
+                    case "l":
+                        foreach (var item in Answers)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.WriteLine("Press anything to continue");
+                        Console.ReadKey();
+                        break;
+
+                }
+
+                var endTime = DateTime.Now - startTime;
+                Console.WriteLine($"It has taken you {endTime.Minutes} minutes {endTime.Seconds} seconds!");
+            }
+        }
+
+        private static void GetComplexityAndAmount()
+        {
             Console.Write("Choose difficulty level (1-10): ");
             DifficultyLevel = int.Parse(Console.ReadLine().Trim());
 
             Console.Write("Choose amount of questions: ");
             NumberOfQuestions = int.Parse(Console.ReadLine().Trim());
 
-            switch (choice.Trim().ToLower())
-            {
-                case "a":
-                    Addition();
-                    break;
-                case "s":
-                    Substraction();
-                    break;
-                case "m":
-                    Multiplication();
-                    break;
-                case "d":
-                    Division();
-                    break;
-                case "q":
-                    Console.WriteLine("Was nice having you here!");
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Wrong input!");
-                    Environment.Exit(1);
-                    break;
-            }
+            Console.WriteLine("Lets go! Time started!");
+            startTime = DateTime.Now;
         }
 
         private static void ShowMenu(DateTime date, string? name)
@@ -55,75 +87,131 @@ A - Addition
 S - Substraction
 M - Multiplication
 D - Division
+R - Random operations
+L - List of all answers
 Q - Quit
 --------------------------------");
         }
 
-        static void Addition()
-        {
+        static void Addition() {
             Console.WriteLine("Addtion chosen!");
+            Addition(NumberOfQuestions);
+        }
+        static void Substraction()
+        {
+            Console.WriteLine("Substraction chosen!");
+            Substraction(NumberOfQuestions);
+        }
+        static void Multiplication()
+        {
+            Console.WriteLine("Multiplication chosen!");
+            Multiplication(NumberOfQuestions);
+        }
+        static void Division()
+        {
+            Console.WriteLine("Division chosen!");
+            Division(NumberOfQuestions);
+        }
+
+        static void Addition(int numberOfTries)
+        {
             var random = new Random();
-            for (int i = 0; i < NumberOfQuestions; i++) {
+            for (int i = 0; i < numberOfTries; i++) {
                 var firstInt = random.Next(1, 5 * DifficultyLevel);
                 var secondInt = random.Next(1, 5 * DifficultyLevel);
                 Console.Write($"{firstInt} + {secondInt} = ");
                 var result = Console.ReadLine();
 
-                if (int.Parse(result.Trim()) == firstInt + secondInt)
+                bool checkIfCorrect = int.Parse(result.Trim()) == firstInt + secondInt;
+
+                if (checkIfCorrect)
                     Console.WriteLine("This is correct!");
                 else
                     Console.WriteLine("This is not correct!");
+                Answers.Add($"{firstInt} + {secondInt} = {result}" + (checkIfCorrect ? "" : $" (Correct answer: {firstInt + secondInt})"));
             }
         }
-        static void Substraction()
+        static void Substraction(int numberOfTries)
         {
-            Console.WriteLine("Substraction chosen!");
             var random = new Random();
-            for (int i = 0; i < NumberOfQuestions; i++)
+            for (int i = 0; i < numberOfTries; i++)
             {
                 var firstInt = random.Next(1, 5 * DifficultyLevel);
                 var secondInt = random.Next(1, 5 * DifficultyLevel);
                 Console.Write($"{firstInt} - {secondInt} = ");
                 var result = Console.ReadLine();
 
-                if (int.Parse(result.Trim()) == firstInt - secondInt)
+                bool checkIfCorrect = int.Parse(result.Trim()) == firstInt - secondInt;
+
+                if (checkIfCorrect)
                     Console.WriteLine("This is correct!");
                 else
                     Console.WriteLine("This is not correct!");
+                Answers.Add($"{firstInt} - {secondInt} = {result}" + (checkIfCorrect ? "" : $" (Correct answer: {firstInt - secondInt})"));
             }
         }
-        static void Multiplication()
+        static void Multiplication(int numberOfTries)
         {
-            Console.WriteLine("Multiplication chosen!");
             var random = new Random();
-            for (int i = 0; i < NumberOfQuestions; i++)
+            for (int i = 0; i < numberOfTries; i++)
             {
                 var firstInt = random.Next(1, 5 * DifficultyLevel);
                 var secondInt = random.Next(1, 5 * DifficultyLevel);
                 Console.Write($"{firstInt} * {secondInt} = ");
                 var result = Console.ReadLine();
 
-                if (int.Parse(result.Trim()) == firstInt * secondInt)
+                bool checkIfCorrect = int.Parse(result.Trim()) == firstInt * secondInt;
+
+                if (checkIfCorrect)
                     Console.WriteLine("This is correct!");
                 else
                     Console.WriteLine("This is not correct!");
+                Answers.Add($"{firstInt} * {secondInt} = {result}" + (checkIfCorrect ? "" : $" (Correct answer: {firstInt * secondInt})"));
             }
         }
-        static void Division()
+        static void Division(int numberOfTries)
         {
-            Console.WriteLine("Division chosen!");
             var random = new Random();
-            for (int i = 0; i < NumberOfQuestions; i++)
+            for (int i = 0; i < numberOfTries; i++)
             {
                 var secondInt = random.Next(1, 5 * DifficultyLevel);
                 var firstInt = secondInt * random.Next(1, 5 * DifficultyLevel);
                 Console.Write($"{firstInt} / {secondInt} = ");
                 var result = Console.ReadLine();
 
-                if (int.Parse(result.Trim()) == firstInt / secondInt)
+                bool checkIfCorrect = int.Parse(result.Trim()) == firstInt / secondInt;
+
+                if (checkIfCorrect)
                     Console.WriteLine("This is correct!");
                 else
                     Console.WriteLine("This is not correct!");
+                Answers.Add($"{firstInt} / {secondInt} = {result}" + (checkIfCorrect ? "" : $" (Correct answer: {firstInt / secondInt})"));
+            }
+        }
+        static void RandomOperators()
+        {
+            Console.WriteLine("Random chosen!");
+            var random = new Random();
+
+            for (int i = 0; i < NumberOfQuestions; i++)
+            {
+                var operation = random.Next(4);
+
+                switch (operation)
+                {
+                    case 0:
+                        Addition(1);
+                        break;
+                    case 1:
+                        Substraction(1);
+                        break;
+                    case 2:
+                        Multiplication(1);
+                        break;
+                    case 3:
+                        Division(1);
+                        break;
+                }
             }
         }
     }
